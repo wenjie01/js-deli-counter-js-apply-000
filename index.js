95a88926b37d4b52ae7da0fbc6d7e482b7b296f3
Wenjie <xuwenjie8866@gmail.com>
2019-10-15 05:45:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function takeANumber(num, name){
-  var katzDeli = [];
+var katzDeli = [];
+function takeANumber(katzDeli, name){
   katzDeli.push(name)
   return "Welcome, " + name + ". You are number "+ katzDeli.length +" in line.";
 }
